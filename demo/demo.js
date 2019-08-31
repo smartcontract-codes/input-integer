5e7cc6a6a49e543b87725f363fd3ac5e7d9d4be6
Fiona Ye <fiona.ye@yandex.com>
2019-08-31 22:02:50 +0800

    Update styling

--- demo/demo.js
+++ demo/demo.js
@@ -2,79 +2,162 @@ const csjs = require('csjs-inject')
 const inputinteger = require('../')
 
 var colors = {
-  transparent: "transparent",
-  white: "#ffffff", // borders, font on input background
-  dark: "#2c323c", //background dark
-  darkSmoke: '#21252b',  // separators
-  whiteSmoke: "#dcd7d7", // background light
-  slateGrey: "#8a929b", // text
-  lightGrey: "#F1F2EB",
-  violetRed: "#fd547d",  // used as red in types (bool etc.)
-  aquaMarine: "#90FCF9",  // used as green in types (bool etc.)
-  turquoise: "#14b9d5",
-  yellow: "#F2CD5D",
-  lavender: "#db94ff",
-  androidGreen: "#9BC53D"
+  bodyFont: `'Nunito', sans-serif`,
+  transparent: 'transparent',
+  inputFieldFontSize: '1.4rem',
+  inputFieldColor: '#ffffff',
+  inputFieldPlaceholderColor: '#8d8d8d',
+  inputFieldBackgroundColor: '#2c323c',
+  inputFieldBorder: 'none',
+  inputFieldBorderRadius: '2px',
+  integerSliderBackgroundColor: '#DCD7D6',
+  integerSliderFocusBackgroundColor: '#ebebeb',
+  integerThumbBackgroundColor: '#00A2F9',
+  integerValueFontSize: '1.4rem',
+  integerValueTextAlign: 'center',
+  integerValueColor: '#ffffff',
+  integerValuePlaceholderColor: '#8d8d8d',
+  integerValueBackgroundColor: '#2c323c',
+  integerValueBorder: 'none',
+  integerValueBorderRadius: '2px',
+  integerValueTextAlign: 'center',
 }
 
 document.body.innerHTML = `<style>
+@import url('https://fonts.googleapis.com/css?family=Nunito&display=swap');
+html {
+  font-size: 62.5%;
+}
+body {
+  font-size: 1.6rem;
+}
 .inputField {
-  ${inputStyle()}
-  font-size: 0.8rem;
-  color: ${colors.whiteSmoke};
-  border-color: ${colors.whiteSmoke};
-  background-color: ${colors.darkSmoke};
-  text-align: center;
-  display: flex;
-  width: 100%;
+  font-family: 'Nunito', sans-serif;
+  font-size: ${colors.inputFieldFontSize};
+  color: ${colors.inputFieldColor};
+  background-color: ${colors.inputFieldBackgroundColor};
+  text-align: ${colors.inputFieldTextAlign};
+  padding: 6px 28px 6px 12px;
+  border-radius: ${colors.inputFieldBorderRadius};
+  border: ${colors.inputFieldBorder};
+  width: calc(100% - 40px);
 }
 .inputField::placeholder {
-  color: ${colors.whiteSmoke};
-  text-align: center;
-  opacity: 0.5;
+  color: ${colors.inputFieldPlaceholderColor};
 }
 .integerValue {
-  ${inputStyle()}
-  font-size: 1rem;
-  color: ${colors.whiteSmoke};
-  background-color: ${colors.darkSmoke};
-  display: flex;
-  text-align: center;
-  width: 25%;
+  width: calc(100% - 42px);
+  font-family: 'Nunito', sans-serif;
+  font-size: ${colors.integerValueFontSize};
+  color: ${colors.integerValueColor};
+  background-color: ${colors.integerValueBackgroundColor};
+  border-radius: ${colors.integerValueBorderRadius};
+  border: ${colors.integerValueBorder};
+  text-align: ${colors.integerValueTextAlign};
+  padding: 6px 30px 6px 12px;
 }
 .integerValue::placeholder {
-  color: ${colors.whiteSmoke};
-  text-align: center;
-  opacity: 0.5;
+  color: ${colors.integerValuePlaceholderColor};
 }
 .integerSlider {
-  width: 75%;
-  border: 1px solid ${colors.whiteSmoke};
   -webkit-appearance: none;
-  height: 0.2px;
+  background-color: transparent;
+  width: 100%;
+  max-width: 100%;  
+  border-radius: 3px;
+  grid-row: 2;
 }
-.integerSlider::-webkit-slider-thumb {
-  -webkit-appearance: none;
-  border: 1px solid ${colors.whiteSmoke};
-  height: 22px;
-  width: 10px;
-  background: ${colors.darkSmoke};
+/* track */
+.integerSlider::-webkit-slider-runnable-track {
+  width: 100%;
+  height: 6px;
+  background-color: ${colors.integerSliderBackgroundColor};
+  border-radius: 3px;
+  grid-row: 2;
+}
+.integerSlider:active::-webkit-slider-runnable-track {
+  background-color: ${colors.integerSliderFocusBackgroundColor};
+}
+.integerSlider::-moz-range-track {
+  width: 100%;
+  height: 6px;
+  background-color: ${colors.integerSliderBackgroundColor};
+  border-radius: 3px;
   cursor: pointer;
 }
+input[type="range"]::-ms-track {
+  width: 100%;
+  height: 6px;
+  cursor: pointer;
+  background: transparent;
+  border-color: transparent;
+  color: transparent;
+}
+input[type="range"]::-ms-fill-lower {
+  background: ${colors.integerSliderBackgroundColor};
+}
+
+input[type="range"]:focus::-ms-fill-lower {
+  background: ${colors.integerSliderFocusBackgroundColor};
+}
+
+input[type="range"]::-ms-fill-upper {
+  background: ${colors.integerSliderBackgroundColor};
+}
+
+input[type="range"]:focus::-ms-fill-upper {
+  background: #ddd;
+}
+/* thumb */
+.integerSlider::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  width: 16px;
+  height: 16px;
+  background-color: ${colors.integerThumbBackgroundColor};
+  border-radius: 50%;
+  box-shadow: 0 0 10px rgba(0, 0, 0, .3);
+  margin-top: -6px;
+  cursor: pointer;
+}
+.integerSlider::-moz-range-thumb {
+  width: 16px;
+  height: 16px;
+  background-color: ${colors.integerThumbBackgroundColor};
+  border-radius: 50%;
+  box-shadow: 0 0 10px rgba(0, 0, 0, .3);
+  cursor: pointer;
+}
+.integerSlider::-ms-range-thumb {
+  width: 16px;
+  height: 16px;
+  background-color: ${colors.integerThumbBackgroundColor};
+  border-radius: 50%;
+  box-shadow: 0 0 10px rgba(0, 0, 0, .3);
+  margin-top: -2px;
+  cursor: pointer;
+}
+.integerSlider:focus {
+  outline: none;
+}
+.integerSlider::-ms-track {
+  width: 100%;
+  border-radius: 3px;
+  cursor: pointer;
+  background: transparent;
+  border-color: transparent;
+  color: transparent;
+}
 .integerField {
-  display: flex;
-  width: 300px;
+  position: relative;
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: auto 40px;
+  grid-column-gap: 5px;
   align-items: center;
+  max-width: 300px;
 }
 </style>`
-
-function inputStyle() {
-  return `
-    border: 1px solid ${colors.whiteSmoke};
-    background-color: ${colors.dark};
-    padding: 5px;
-  `
-}
 
 const classes = {
   inputField: 'inputField',
